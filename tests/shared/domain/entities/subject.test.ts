01129f9a87836ef89b249c2dcca7846318ac9d43
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-10-05 11:15:37 -0300

    refat: simplify tests

--- tests/shared/domain/entities/subject.test.ts
+++ tests/shared/domain/entities/subject.test.ts
@@ -6,8 +6,8 @@ import { EntityError } from '../../../../src/shared/helpers/errors/domain_errors
 describe('Subject Entity Tests', () => {
   it('Assert Subject Entity is correct at all', () => {
     const subject = new Subject({
-      code: 'MAT123',
-      name: 'Matemática',
+      code: 'ECM256',
+      name: 'Linguagens de Programacao II',
       period: PERIOD.MORNING,
     })
 
@@ -17,43 +17,29 @@ describe('Subject Entity Tests', () => {
   it('Assert Subject Entity has an error when code is invalid', () => {
     expect(() => {
       new Subject({
-        code: 'MAT',
-        name: 'Matemática',
+        code: 'ECM',
+        name: 'Linguagens de Programacao II',
         period: PERIOD.AFTERNOON,
       })
     }).toThrowError(EntityError)
-    expect(() => {
-      new Subject({
-        code: 'MAT',
-        name: 'Matemática',
-        period: PERIOD.AFTERNOON,
-      })
-    }).toThrowError('Field props.code is not valid')
   })
 
   it('Assert Subject Entity has an error when name is invalid', () => {
     expect(() => {
       new Subject({
-        code: 'MAT123',
+        code: 'ECM256',
         name: '',
         period: PERIOD.MORNING,
       })
     }).toThrowError(EntityError)
-    expect(() => {
-      new Subject({
-        code: 'MAT123',
-        name: '',
-        period: PERIOD.MORNING,
-      })
-    }).toThrowError('Field props.name is not valid')
-  })
 
-  it('Assert Subject Entity has an error when period is invalid', () => {
-    expect(() => {
-      new Subject({
-        code: 'MAT123',
-        name: 'Matemática',
-      })
-    }).toThrowError(EntityError)
+    it('Assert Subject Entity has an error when period is invalid', () => {
+      expect(() => {
+        new Subject({
+          code: 'ECM256',
+          name: 'Linguagens de Programacao II',
+        })
+      }).toThrowError(EntityError)
+    })
   })
 })
