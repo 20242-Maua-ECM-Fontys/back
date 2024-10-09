c53b8298155ff7840d1e613512d078dfec4456f5
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-10-08 23:26:33 -0300

    chore(error): add Invalid CSV format error
    Co-authored-by: João Vitor Choueri Branco <JoaoVitorBranco@users.noreply.github.com>

--- src/shared/helpers/errors/usecase_errors.ts
+++ src/shared/helpers/errors/usecase_errors.ts
@@ -4,14 +4,18 @@ export class NoItemsFound extends BaseError {
   constructor(message: string) {
     super(`No items found for ${message}`)
   }
-
 }
 
 export class DuplicatedItem extends BaseError {
   constructor(message: string) {
     super(`The item already exists for this ${message}`)
   }
+}
 
+export class InvalidCSVFormat extends BaseError {
+  constructor(entity: string) {
+    super(`CSV file with invalid format on entity ${entity}`)
+  }
 }
 
 export class ForbiddenAction extends BaseError {
