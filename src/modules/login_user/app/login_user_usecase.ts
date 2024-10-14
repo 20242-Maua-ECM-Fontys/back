c1f0fba8d5d72419eebb44a41acd82a981ea50b3
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-10-14 08:28:25 -0300

    fix(login): fix usecase error messages

--- src/modules/login_user/app/login_user_usecase.ts
+++ src/modules/login_user/app/login_user_usecase.ts
@@ -7,21 +7,17 @@ export class LoginUserUsecase {
   constructor(private repo: IUserRepository) {}
 
   async execute(email: string, password: string) {
-
-    if (!email || !password) {
-      throw new EntityError('Email and password are required')
-    }
     if (!User.validatePassword(password)) {
-      throw new EntityError('Invalid password format')
+      throw new EntityError('password')
     }
     if (!User.validateEmail(email)) {
-      throw new EntityError('Invalid email format')
+      throw new EntityError('email')
     }
 
     const user = await this.repo.loginUser(email, password)
 
     if (!user) {
-      throw new NoItemsFound('Invalid email or password') 
+      throw new NoItemsFound('email')
     }
 
     return user
