e9d1dac3c3c7fedea936a2196e6edf26ac6bd03d
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-10-14 09:07:52 -0300

    feat(repo): add getLength to repository interface

--- src/shared/domain/repositories/user_repository_interface.ts
+++ src/shared/domain/repositories/user_repository_interface.ts
@@ -4,7 +4,7 @@ export interface IUserRepository {
   getUser(id: number): Promise<User>
   getAllUsers(): Promise<User[]>
   createUser(user: User): Promise<User>
-
+  getLength(): number
   updateUser(id: number, newName: string, newEmail: string): Promise<User>
   deleteUser(id: number): Promise<User>
   loginUser(email: string, password: string): Promise<User | null>
