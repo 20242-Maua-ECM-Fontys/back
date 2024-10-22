e3e9f6c36198a01737e1c9f21dc4623c35925b43
Joao Branco <21.01075-7@maua.br>
2024-10-22 10:02:32 -0300

    feat(get_all_subjects): create viewmodel with unit test

--- tests/modules/get_all_subjets/app/get_all_subjects_viewmodel.test.ts
+++ tests/modules/get_all_subjets/app/get_all_subjects_viewmodel.test.ts
@@ -1,1 +1,43 @@
 
+import { it, expect, describe } from 'vitest'
+import { GetAllSubjectsViewmodel } from '../../../../src/modules/get_all_subjects/app/get_all_subjects_viewmodel'
+import { GetAllSubjectsUsecase } from '../../../../src/modules/get_all_subjects/app/get_all_subjects_usecase'
+import { ScheduleRepositoryMock } from '../../../../src/shared/infra/repositories/schedule_repository_mock'
+
+describe('Assert Get All Subjects viewmodel is correct', () => {
+  it('Should correctly transform user data to viewmodel', async () => {
+
+    const repo = new ScheduleRepositoryMock()
+    const usecase = new GetAllSubjectsUsecase(repo)
+    const subjects = await usecase.execute()
+    const getAllSubjectsViewmodel = new GetAllSubjectsViewmodel(subjects).toJSON()
+
+    expect(getAllSubjectsViewmodel).toEqual({
+      "message": "subjects returned",
+      "subjects":  [
+        {
+          "codeSubject": "ECM256",
+          "period": "MORNING",
+          "subjectName": "Programming Languages II",
+        },
+        {
+          "codeSubject": "EFB207",
+          "period": "AFTERNOON",
+          "subjectName": "Physics I",
+        },
+      ],
+    })
+  })
+    it('Should correctly transform user data to viewmodel if list is empty', async () => {
+
+      const getAllSubjectsViewmodel = new GetAllSubjectsViewmodel([]).toJSON()
+  
+      expect(getAllSubjectsViewmodel).toEqual({
+        "message": "subjects returned",
+        "subjects":  [
+        ],
+      })
+  })
+})
+
+  
