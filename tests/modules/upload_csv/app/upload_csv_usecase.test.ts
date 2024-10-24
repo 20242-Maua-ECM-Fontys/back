764b555739a041db44bea29680d0a38caf999b78
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-10-24 16:49:26 -0300

    test(csv): add scheduleId test on uploadcsv

--- tests/modules/upload_csv/app/upload_csv_usecase.test.ts
+++ tests/modules/upload_csv/app/upload_csv_usecase.test.ts
@@ -172,4 +172,23 @@ class,Class 202,REMOTE,LAB,CSE204,,A02,,,,,2S-4CM-D5@2024(SCS)`
       'user must be a coordinator',
     )
   })
+
+  it('Should raise error for invalid schedulaId', async () => {
+    const repo = new ScheduleRepositoryMock()
+    const usecase = new UploadCSVUsecase(repo)
+
+    const csvContent = `type,name,classModality,classType,subjectCode,subjectPeriod,roomCode,professorEmail,professorRa,professorPassword,roomCode,scheduleId,courseName,coordEmail,academicPeriod
+schedule,,,,,,,,,,,2222),Computer Science,user2@gmail.com,ANNUAL
+professor,Dr. John Doe,,,,,,john.doe@example.com,12345,S!q3T@pG9z,,
+subject,Data Structures,,,CSE103,EVENING,,,,,,
+class,Class 101,HYBRID,THEORY,CSE104,,A01,,,,,2S-4CM-D5@2024(SCS)
+professor,Dr. Jane Smith,,,,,,jane.smith@example.com,54321,Y@uP!zG6rX,,
+subject,Algorithms,,,CSE203,AFTERNOON,,,,,,
+class,Class 202,REMOTE,LAB,CSE204,,A02,,,,,2S-4CM-D5@2024(SCS)`
+    const csvBuffer: Buffer = Buffer.from(csvContent, 'utf-8')
+
+    await expect(usecase.execute(csvBuffer)).rejects.toThrowError(
+      'Field scheduleId is not valid',
+    )
+  })
 })
