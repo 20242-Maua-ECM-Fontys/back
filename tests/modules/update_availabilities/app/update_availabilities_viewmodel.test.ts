6703bf59f6e9e59147cc1e534cc937a04a05c068
Joao Branco <21.01075-7@maua.br>
2024-10-25 10:35:21 -0300

    fix(update_availabilities_viewmodel): fix test

--- tests/modules/update_availabilities/app/update_availabilities_viewmodel.test.ts
+++ tests/modules/update_availabilities/app/update_availabilities_viewmodel.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { ScheduleRepositoryMock } from '../../../../src/shared/infra/repositories/schedule_repository_mock'
+import { UpdateAvailabilitiesViewmodel } from '../../../../src/modules/update_availabilities/app/update_availabilities_viewmodel'
 
 describe('Assert UpdateAvailabilitiesViewmodel is correct', () => {
   it('Should correctly transform user data to viewmodel', async () => {
