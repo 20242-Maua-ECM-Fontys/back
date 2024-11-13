9745097b088c8b7945d265950a772ebe388491c1
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-11-13 20:17:21 -0300

    feat(get_schedule_by_coordinator): add class fullfilled Data to viewmodel

--- src/modules/get_schedules_by_coordinator/app/get_schedules_by_coordinator_viewmodel.ts
+++ src/modules/get_schedules_by_coordinator/app/get_schedules_by_coordinator_viewmodel.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { completeSchedule } from './get_schedules_by_coordinator_usecase'
 
 export class GetSchedulesByCoordinatorViewmodel {
@@ -10,33 +11,52 @@ export class GetSchedulesByCoordinatorViewmodel {
   }
 
   toJSON() {
-    return {
-      message: this.message,
-      schedules: this.schedules.map((schedule) => ({
-        schedule: {
-          id: schedule.schedule.scheduleId,
-          userId: schedule.schedule.userId,
+    const schedulesById = this.schedules.reduce(
+      (acc, schedule) => {
+        const classesById = schedule.classes.reduce(
+          (classAcc, class_) => {
+            classAcc[class_.id] = {
+              name: class_.name,
+              subjectCode: class_.subjectCode,
+              modality: class_.modality,
+              classType: class_.classType,
+              ...(class_.fullfilledData && {
+                fullfilledData: class_.fullfilledData,
+              }),
+            }
+            return classAcc
+          },
+          {} as Record<string, any>,
+        )
+
+        const possibilitiesById = schedule.possibilities.reduce(
+          (possAcc, possibility) => {
+            possAcc[possibility.id] = {
+              weekDay: possibility.weekDay,
+              startTime: possibility.startTime,
+              endTime: possibility.endTime,
+            }
+            return possAcc
+          },
+          {} as Record<string, any>,
+        )
+
+        acc[schedule.schedule.scheduleId] = {
+          academicPeriod: schedule.schedule.academicPeriod,
           courseName: schedule.schedule.courseName,
           groupNumber: schedule.schedule.groupNumber,
-          academicPeriod: schedule.schedule.academicPeriod,
           courseGrade: schedule.schedule.courseGrade,
-          classes: schedule.classes.map((class_) => ({
-            id: class_.id,
-            name: class_.name,
-            modality: class_.modality,
-            classType: class_.classType,
-            subjectCode: class_.subjectCode,
-            scheduleId: class_.scheduleId,
-          })),
-          possibilities: schedule.possibilities.map((possibility) => ({
-            id: possibility.id,
-            weekDay: possibility.weekDay,
-            startTime: possibility.startTime,
-            endTime: possibility.endTime,
-            scheduleId: possibility.scheduleId,
-          })),
-        },
-      })),
+          possibilities: possibilitiesById,
+          classes: classesById,
+        }
+        return acc
+      },
+      {} as Record<string, any>,
+    )
+
+    return {
+      message: this.message,
+      schedules: schedulesById,
     }
   }
 }
