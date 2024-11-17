37e3493754ef6fc12324238f5f2e60854e65f645
Joao Branco <21.01075-7@maua.br>
2024-11-17 20:00:13 -0300

    feat(update_availabilities_fullfilled): create presenter with unit tests

--- src/modules/update_availabilities_fullfilled/app/update_availabilities_fullfilled_presenter.ts
+++ src/modules/update_availabilities_fullfilled/app/update_availabilities_fullfilled_presenter.ts
@@ -1,15 +1,15 @@
-import { UpdateAvailabilitiesController } from './update_availabilities_fullfilled_controller'
-import { UpdateAvailabilitiesUsecase } from './update_availabilities_fullfilled_usecase'
+import { UpdateAvailabilitiesFullfilledController } from './update_availabilities_fullfilled_controller'
+import { UpdateAvailabilitiesFullfilledUsecase } from './update_availabilities_fullfilled_usecase'
 import { HttpResponse } from '../../../shared/helpers/external_interfaces/http_models'
 import { IRequest } from '../../../shared/helpers/external_interfaces/external_interface'
 import { IScheduleRepository } from '../../../shared/domain/repositories/schedule_repository_interface'
 
-export async function UpdateAvailabilitiesPresenter(
+export async function UpdateAvailabilitiesFullfilledPresenter(
   event: IRequest,
   repo: IScheduleRepository,
 ): Promise<HttpResponse> {
-  const usecase = new UpdateAvailabilitiesUsecase(repo)
-  const controller = new UpdateAvailabilitiesController(usecase)
+  const usecase = new UpdateAvailabilitiesFullfilledUsecase(repo)
+  const controller = new UpdateAvailabilitiesFullfilledController(usecase)
   const response = await controller.execute(event)
   const httpResponse = new HttpResponse(response?.statusCode, response?.body)
 
--- tests/modules/update_availabilities_fullfilled/app/update_availabilities_fullfilled_presenter.test.ts
+++ tests/modules/update_availabilities_fullfilled/app/update_availabilities_fullfilled_presenter.test.ts
@@ -1,142 +1,172 @@
 import { describe, it, expect } from 'vitest'
-import { UpdateAvailabilitiesPresenter } from '../../../../src/modules/update_availabilities/app/update_availabilities_presenter'
+import { UpdateAvailabilitiesFullfilledPresenter } from '../../../../src/modules/update_availabilities_fullfilled/app/update_availabilities_fullfilled_presenter'
 import { HttpRequest } from '../../../../src/shared/helpers/external_interfaces/http_models'
 import { ScheduleRepositoryMock } from '../../../../src/shared/infra/repositories/schedule_repository_mock'
+import { AvailabilitiesFullfilledParam } from '../../../../src/modules/update_availabilities_fullfilled/app/update_availabilities_fullfilled_usecase'
 
-describe('Tests for UpdateAvailabilitiesPresenter', () => {
-  it('Should call presenter and return status 200', async () => {
+describe('Tests for UpdateAvailabilitiesFullfilledPresenter', () => {
+  it('Should call presenter and return OK with status 200', async () => {
     const repo = new ScheduleRepositoryMock()
-    const userId = 3
-    const availabilities = [
+    const scheduleId = "2S-4CM-D5@2024(SCS)"
+    const availabilitiesFullfilled : AvailabilitiesFullfilledParam[] = [
       {
-        startTime: 460,
-        endTime: 560,
-        weekDay: 'FRI'
+        userId:3,
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac926",
+        possibilityId:"124e4567-e89b-12d3-a456-426614174000"
       },
       {
-        startTime: 460,
-        endTime: 560,
-        weekDay: 'SAT'
+        userId:3,
+        possibilityId:"123e4567-e89b-12d3-a456-426614174001",
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac927"
       },
       {
-        startTime: 570,
-        endTime: 670,
-        weekDay: 'FRI'
+        userId: 3,
+        possibilityId: '113e4567-e89b-12d3-a456-426614174000',
+        classId: '0a8c5357-1f07-5b24-9845-9318c47ac924',
       }
     ]
     const httpRequest = new HttpRequest(
       {
-        'userId': userId,
-        'availabilities': availabilities
+        'scheduleId': scheduleId,
+        'availabilitiesFullfilled': availabilitiesFullfilled
       },
       undefined,
       {},
       undefined,
     )
-    const response = await UpdateAvailabilitiesPresenter(httpRequest, repo)
+    const response = await UpdateAvailabilitiesFullfilledPresenter(httpRequest, repo)
 
     expect(response?.statusCode).toEqual(200)
-    expect(response?.body.message).toEqual('availabilities updated')
+    expect(response?.body.message).toEqual('availabilities fullfilled updated')
   })
-  it('Should call presenter and return status 400', async () => {
+  it('Should call presenter and return Bad Request with status 400', async () => {
     const repo = new ScheduleRepositoryMock()
-    const userId = 3
-    const availabilities = [
+    const scheduleId = "2S-4CM-D5@2024(SCS)"
+    const availabilitiesFullfilled : AvailabilitiesFullfilledParam[] = [
       {
-        startTime: 460,
-        weekDay: 'FRI'
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac926",
+        possibilityId:"124e4567-e89b-12d3-a456-426614174000"
       },
       {
-        startTime: 460,
-        endTime: 560,
-        weekDay: 'SAT'
+        userId:3,
+        possibilityId:"123e4567-e89b-12d3-a456-426614174001",
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac927"
       },
       {
-        startTime: 570,
-        endTime: 670,
-        weekDay: 'FRI'
+        userId: 3,
+        possibilityId: '113e4567-e89b-12d3-a456-426614174000',
+        classId: '0a8c5357-1f07-5b24-9845-9318c47ac924',
       }
     ]
     const httpRequest = new HttpRequest(
       {
-        'userId': userId,
-        'availabilities': availabilities
+        'scheduleId': scheduleId,
+        'availabilitiesFullfilled': availabilitiesFullfilled
       },
       undefined,
       {},
       undefined,
     )
-    const response = await UpdateAvailabilitiesPresenter(httpRequest, repo)
+    const response = await UpdateAvailabilitiesFullfilledPresenter(httpRequest, repo)
 
     expect(response?.statusCode).toEqual(400)
-    expect(response?.body).toEqual('Field endTime is missing')
+    expect(response?.body).toEqual('Field userId is missing')
   })
-  it('Should call presenter and return status 403', async () => {
+  it('Should call presenter and return Forbidden with status 403', async () => {
     const repo = new ScheduleRepositoryMock()
-    const userId = 1
-    const availabilities = [
+    const scheduleId = "2S-4CM-D5@2024(SCS)"
+    const availabilitiesFullfilled : AvailabilitiesFullfilledParam[] = [
       {
-        startTime: 460,
-        endTime: 560,
-        weekDay: 'FRI'
+        userId:3,
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac926",
+        possibilityId:"124e4567-e89b-12d3-a456-426614174000"
       },
       {
-        startTime: 460,
-        endTime: 560,
-        weekDay: 'SAT'
+        userId:1,
+        possibilityId:"123e4567-e89b-12d3-a456-426614174001",
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac927"
       },
       {
-        startTime: 570,
-        endTime: 670,
-        weekDay: 'FRI'
+        userId: 3,
+        possibilityId: '113e4567-e89b-12d3-a456-426614174000',
+        classId: '0a8c5357-1f07-5b24-9845-9318c47ac924',
       }
     ]
     const httpRequest = new HttpRequest(
       {
-        'userId': userId,
-        'availabilities': availabilities
+        'scheduleId': scheduleId,
+        'availabilitiesFullfilled': availabilitiesFullfilled
       },
       undefined,
       {},
       undefined,
     )
-    const response = await UpdateAvailabilitiesPresenter(httpRequest, repo)
+    const response = await UpdateAvailabilitiesFullfilledPresenter(httpRequest, repo)
 
     expect(response?.statusCode).toEqual(403)
-    expect(response?.body).toEqual('Invalid role. Expected PROFESSOR or COORDINATOR but received STAFF')
+    expect(response?.body).toEqual('The data rule "user must be a professor" was violated')
   })
-  it('Should call presenter and return status 404', async () => {
+  it('Should call presenter and return NotFound with status 404', async () => {
     const repo = new ScheduleRepositoryMock()
-    const userId = repo.getUsersLength() + 1
-    const availabilities = [
+    const scheduleId = "2S-4CM-D5@2024(SCS)"
+    const availabilitiesFullfilled : AvailabilitiesFullfilledParam[] = [
       {
-        startTime: 460,
-        endTime: 560,
-        weekDay: 'FRI'
+        userId:await repo.getUsersLength() + 1,
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac926",
+        possibilityId:"124e4567-e89b-12d3-a456-426614174000"
       },
       {
-        startTime: 460,
-        endTime: 560,
-        weekDay: 'SAT'
+        userId:3,
+        possibilityId:"123e4567-e89b-12d3-a456-426614174001",
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac927"
       },
       {
-        startTime: 570,
-        endTime: 670,
-        weekDay: 'FRI'
+        userId: 3,
+        possibilityId: '113e4567-e89b-12d3-a456-426614174000',
+        classId: '0a8c5357-1f07-5b24-9845-9318c47ac924',
       }
     ]
     const httpRequest = new HttpRequest(
       {
-        'userId': userId,
-        'availabilities': availabilities
+        'scheduleId': scheduleId,
+        'availabilitiesFullfilled': availabilitiesFullfilled
       },
       undefined,
       {},
       undefined,
     )
-    const response = await UpdateAvailabilitiesPresenter(httpRequest, repo)
+    const response = await UpdateAvailabilitiesFullfilledPresenter(httpRequest, repo)
 
     expect(response?.statusCode).toEqual(404)
     expect(response?.body).toEqual('No items found for userId')
+  })
+  it('Should call presenter and return Conflict with status 409', async () => {
+    const repo = new ScheduleRepositoryMock()
+    const scheduleId = "2S-4CM-D5@2024(SCS)"
+    const availabilitiesFullfilled : AvailabilitiesFullfilledParam[] = [
+      {
+        userId:3,
+        possibilityId:"125e4567-e89b-12d3-a456-426614174001",
+        classId:"0a8c5357-1f07-5b24-9845-9318c47ac926"
+      },
+      {
+        userId: 3,
+        possibilityId: '113e4567-e89b-12d3-a456-426614174000',
+        classId: '0a8c5357-1f07-5b24-9845-9318c47ac924',
+      }
+    ]
+    const httpRequest = new HttpRequest(
+      {
+        'scheduleId': scheduleId,
+        'availabilitiesFullfilled': availabilitiesFullfilled
+      },
+      undefined,
+      {},
+      undefined,
+    )
+    const response = await UpdateAvailabilitiesFullfilledPresenter(httpRequest, repo)
+
+    expect(response?.statusCode).toEqual(409)
+    expect(response?.body).toEqual("The professor with id 3 is already assigned to schedule with id 2S-3CM-D5@2024(SCS)")
   })
 })
